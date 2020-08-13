c7ebeb82c558938ef9b9dbfc16a5f28021aff7b3
root <david.ball.e@hotmail.com>
2020-08-13 01:13:03 +0000

    agregando puerto

--- node.js
+++ node.js
@@ -1,2 +1,11 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (requ,resp)=>{
+ res.send({msg: "Mi primera prueba"});
+});
+
+
+app.listen(5000, () => console.log("servidor coriendo"));
 console.log("Hola mundo desde CI/CD usando GithubActions");
 
